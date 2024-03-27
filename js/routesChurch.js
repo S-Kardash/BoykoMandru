cbf96721620dc6c826a3c797ea381722acacf6a7
S-Kardash <kardashsashaa2004@gmail.com>
2024-03-27 15:58:57 +0200

    adding page "reach", remove animation "bottom-row"

--- js/routesChurch.js
+++ js/routesChurch.js
@@ -1,72 +1,63 @@
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
 let lastScrollPosition = window.scrollY;
 
 window.onscroll = () => {
-    const currentScrollPosition = window.scrollY;
+	const currentScrollPosition = window.scrollY;
 
-    if (currentScrollPosition > lastScrollPosition) {
-        // Прокрутка вниз: ховаємо хедер плавно
-        header.style.transition = 'opacity 0.5s ease';
-        header.style.visibility = "hidden"
-        header.style.opacity = 0;
-    } else {
-        // Прокрутка вгору: показуємо хедер плавно
-        header.style.transition = 'opacity 0.9s ease';
-        header.style.opacity = 1;
-        header.style.visibility = "visible";
-    }
+	if (currentScrollPosition > lastScrollPosition) {
+		// Прокрутка вниз: ховаємо хедер плавно
+		header.style.transition = "opacity 0.5s ease";
+		header.style.visibility = "hidden";
+		header.style.opacity = 0;
+	} else {
+		// Прокрутка вгору: показуємо хедер плавно
+		header.style.transition = "opacity 0.9s ease";
+		header.style.opacity = 1;
+		header.style.visibility = "visible";
+	}
 
-    lastScrollPosition = currentScrollPosition;
+	lastScrollPosition = currentScrollPosition;
 };
 
-const elementsScroll = document.querySelectorAll('.animate-on-scroll');
+const elementsScroll = document.querySelectorAll(".animate-on-scroll");
 
 const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            // Виконати анімацію, коли елемент стає видимим
-            entry.target.style.opacity = 1;
-            entry.target.style.transform = 'translateY(0)';
-        }
-    });
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			// Виконати анімацію, коли елемент стає видимим
+			entry.target.style.opacity = 1;
+			entry.target.style.transform = "translateY(0)";
+		}
+	});
 });
 
 elementsScroll.forEach((element) => {
-    observer.observe(element);
+	observer.observe(element);
 });
 
 const closeButton = document.getElementById("close-icon");
-const openButton = document.getElementById("menu")
+const openButton = document.getElementById("menu");
 const blockToClose = document.getElementById("mobile-header");
 const blockToClose1 = document.getElementById("container-header");
 
 openButton.addEventListener("click", function () {
-    // Змініть стиль блоку, щоб його приховати (змініть display на "none")
-    blockToClose.style.display = "flex";
-    blockToClose1.style.display = "none";
+	// Змініть стиль блоку, щоб його приховати (змініть display на "none")
+	blockToClose.style.display = "flex";
+	blockToClose1.style.display = "none";
 });
 
 // Додайте обробник події на кнопку
 closeButton.addEventListener("click", function () {
-    // Змініть стиль блоку, щоб його приховати (змініть display на "none")
-    blockToClose1.style.display = "flex";
-    blockToClose.style.display = "none";
+	// Змініть стиль блоку, щоб його приховати (змініть display на "none")
+	blockToClose1.style.display = "flex";
+	blockToClose.style.display = "none";
 });
 
-// var rotateImage = document.getElementById('rotateImage');
+var rotateImage = document.getElementById("rotateImage");
 
-// rotateImage.addEventListener('click', function () {
-//     rotateImage.classList.toggle('rotate');
-// });
-
-var rotateImage = document.getElementById('rotateImage');
-var hiddenElement = document.getElementById('hiddenElement');
-
-rotateImage.addEventListener('click', function () {
-    rotateImage.classList.toggle('rotate');
-    // Додайте або видаліть клас visible для визначення видимості
-    hiddenElement.classList.toggle('visible');
+rotateImage.addEventListener("click", function () {
+	rotateImage.classList.toggle("rotate");
 });
 
 // let containerVisible = false;
@@ -109,32 +100,31 @@ rotateImage.addEventListener('click', function () {
 //     containerVisible = !containerVisible;
 // }
 
-
 let containerVisible = false;
-const container = document.getElementById('container');
-const elements = document.querySelectorAll('.element');
+const container = document.getElementById("container");
+const elements = document.querySelectorAll(".element");
 
 function toggleContainer() {
-    container.classList.toggle('visible', !containerVisible);
+	container.classList.toggle("visible", !containerVisible);
 
-    if (!containerVisible) {
-        container.style.maxHeight = '7372px';
-        container.classList.remove('hidden');
-        elements.forEach((element, index) => {
-            const delay = index * 0.1;
-            element.style.transition = `transform 0.5s ease-in-out ${delay}s, opacity 0.5s ease-in-out ${delay}s`;
-            element.style.opacity = 1;
-            element.style.transform = 'translateY(0)';
-        });
-    } else {
-        container.style.maxHeight = '0';
-        elements.forEach((element, index) => {
-            const delay = index * 0.1;
-            element.style.transition = `transform 0.5s ease-in-out ${delay}s, opacity 0.5s ease-in-out ${delay}s`;
-            element.style.opacity = 0;
-            element.style.transform = 'translateY(20px)';
-        });
-    }
+	if (!containerVisible) {
+		container.style.maxHeight = "7372px";
+		container.classList.remove("hidden");
+		elements.forEach((element, index) => {
+			const delay = index * 0.1;
+			element.style.transition = `transform 0.5s ease-in-out ${delay}s, opacity 0.5s ease-in-out ${delay}s`;
+			element.style.opacity = 1;
+			element.style.transform = "translateY(0)";
+		});
+	} else {
+		container.style.maxHeight = "0";
+		elements.forEach((element, index) => {
+			const delay = index * 0.1;
+			element.style.transition = `transform 0.5s ease-in-out ${delay}s, opacity 0.5s ease-in-out ${delay}s`;
+			element.style.opacity = 0;
+			element.style.transform = "translateY(20px)";
+		});
+	}
 
-    containerVisible = !containerVisible;
+	containerVisible = !containerVisible;
 }
